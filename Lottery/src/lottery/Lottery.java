4f97fc5b589241c2a8b7fa62dbb3dd1f48b7dd8c
Gigatweak <gigafry665@gmail.com>
2018-03-22 22:02:31 -0500

    Add comments to code

--- Lottery/src/lottery/Lottery.java
+++ Lottery/src/lottery/Lottery.java
@@ -7,24 +7,32 @@ you need to keep track of the times it took to get it run successfully with no d
 public class Lottery {
 
     public static void main(String[] args) {
-    
+    	//Begin variable section
+	//Counter variable
         int i = 1;
+	//Variable for 1st random number
         int a;
+	//Variable for 2nd random number
         int b;
+	//Variable for 3rd random number
         int c;
+	//Variable for 4th random number
         int d;
 
+	//Assign variables to random numbers
         a = getRandomInteger(9,1);
         b = getRandomInteger(9,1);
         c = getRandomInteger(9,1);
         d = getRandomInteger(9,1);
   
+	//Print statement of 1st try
         System.out.println("Lottery #1 is " + a +
                                "\nLottery #2 is " + b +
                                "\nLottery #3 is " + c + 
                                "\nLottery #4 is " + d +
                                "\nHow many tries? " + (i));
         
+	//While loop if 1st try fails
         while(a==b || a==c || a==d || b==c || b==d || c==d){
             a = getRandomInteger(9,1);
             b = getRandomInteger(9,1);
@@ -32,6 +40,7 @@ public class Lottery {
             d = getRandomInteger(9,1);
             i++;
             
+	    //Same print statement as 1st print statement
             System.out.println("\nLottery #1 is " + a +
                                "\nLottery #2 is " + b +
                                "\nLottery #3 is " + c + 
@@ -39,6 +48,8 @@ public class Lottery {
                                "\nHow many tries? " + (i));
         }
     }
+    //getRandomInteger constructor
+    //This constructor generates a random number between 2 values which are declared at call
     public static int getRandomInteger(int max, int min){
         return ((int) (Math.random()*(max - min))) + min;
     }
